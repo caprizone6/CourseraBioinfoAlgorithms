05ffba2d4484bc15ad8389f32139896e9458a429
jaimin patel <caprizone6@gmail.com>
2019-12-11 19:43:16 +0000

    Initial commit

---
+++ week2.py
@@ -0,0 +1,132 @@
+import week1
+
+def Skew(text):
+  SkewVals = []
+  SkewVal = 0
+  SkewVals.append(SkewVal)
+  dict = {"A": 0, "C": -1, "G": 1, "T": 0}
+  for i, nt in enumerate(list(text)):
+    SkewVal = SkewVal + dict[nt]
+    SkewVals.append(SkewVal)
+  return SkewVals
+
+
+def MinSkewPos(text):
+  SkewVals = Skew(text)
+  MinSkewPos = []
+  minSKew = min(SkewVals)
+  for i, val in enumerate(SkewVals):
+    if val==minSKew:
+      MinSkewPos.append(str(i))
+  return ' '.join(MinSkewPos)
+
+def HammingDistance(p,q):
+  hamDist = 0
+  for i, nt in enumerate(p[::-1]):
+    if(p[i]!=q[i]):
+      hamDist += 1
+  return hamDist
+
+def ImmediateNeighbors(Pattern):
+  Neighborhood = [Pattern]
+  nt = ['A','C','G','T']
+  for i in range (len(Pattern)):
+    symbol = Pattern[i]
+    for j in range(len(nt)):
+      if nt[j] != symbol:
+        PatternArry = list(Pattern)
+        PatternArry[i] = nt[j]
+        Neighbor = ''.join(PatternArry)
+        Neighborhood.append(Neighbor) 
+  return Neighborhood
+
+
+def Neighbors(Pattern, d):
+  nt = ['C','T','G','A']
+  Neighborhood = []
+  if d == 0:
+    return {Pattern}
+  if len(Pattern) == 1: 
+    return nt
+  SuffixNeighbors = Neighbors(Pattern[1:], d)
+  for i, Text in enumerate(SuffixNeighbors):
+      if (HammingDistance(Pattern[1:], Text) < d):
+        for j in range(len(nt)):
+          Neighborhood.append(nt[j] + Text)
+      else:
+        Neighborhood.append(Pattern[0:1] + Text)      
+  return Neighborhood
+
+def AproximatePatternStart(Pattern, Text, d):
+  starts = []
+  itr = len(Text) - len(Pattern) + 1
+  for i in range(itr):
+    if HammingDistance(Text[i: i+len(Pattern)], Pattern) <= d :
+      starts.append(str(i)) 
+  return ' '.join(starts)
+
+def AproximatePatternCount(Pattern, Text, d):
+  count = 0
+  itr = len(Text) - len(Pattern) + 1
+  for i in range(itr):
+    if HammingDistance(Text[i: i+len(Pattern)], Pattern) <= d :
+      count= count+1
+  return count
+
+def FrequentWordsWithMismatches(Text, k, d):
+  FrequentPatterns = []
+  NeighborhoodArray = []
+  Index = []
+  Count = []
+
+  for i in range(len(Text) - k+1): 
+    Nbrs = Neighbors(Text[i:i+k], d)
+    for j, Nbr in enumerate(Nbrs):
+      NeighborhoodArray.append(Nbr)
+
+  for i in range(len(NeighborhoodArray)):
+    Pattern = NeighborhoodArray[i] 
+    Index.append(week1.PatternToNumber(Pattern))
+    Count.append(1)
+
+  SortedIndex = sorted(Index)
+
+  for i in range (len(NeighborhoodArray) - 1): 
+    if SortedIndex[i] == SortedIndex[i + 1]:
+      Count[i + 1] = Count[i] + 1
+
+  maxCount = max(Count)
+
+  for i in range(len(NeighborhoodArray)):
+    if Count[i] == maxCount:
+      Pattern = week1.NumberToPattern(SortedIndex[i], k)
+      FrequentPatterns.append(Pattern)
+
+  return ' '.join(FrequentPatterns)
+
+def FrequentWordsWithMismatchesRevcomp(Text, k, d):
+  FrequentPatterns = []
+  NeighborhoodArray = []
+  Index = []
+  Count = []
+
+  for i in range(len(Text) - k+1): 
+    Nbrs = Neighbors(Text[i:i+k], d)
+    for j, Nbr in enumerate(Nbrs):
+      NeighborhoodArray.append(Nbr)
+
+  NeighborhoodArray = list(dict.fromkeys(NeighborhoodArray))
+
+  for i in range(len(NeighborhoodArray)):
+    Pattern = NeighborhoodArray[i] 
+    Index.append(week1.PatternToNumber(Pattern))
+    Count.append(AproximatePatternCount(Pattern, Text, d) + AproximatePatternCount(week1.RevComp(Pattern),Text,d))
+
+  maxCount = max(Count)
+
+  for i in range(len(NeighborhoodArray)):
+    if Count[i] == maxCount:
+      Pattern = week1.NumberToPattern(Index[i], k)
+      FrequentPatterns.append(Pattern)
+
+  return ' '.join(FrequentPatterns)
---
+++ week3.py
@@ -0,0 +1,143 @@
+import week1
+import week2
+import math
+
+def MotifEnumeration(Dna, k, d):
+  Patterns = []
+  for i in range(len(Dna[0])-k+1):
+    Text = Dna[0][i:i+k]
+    for j, Pattern in enumerate(week2.Neighbors(Text,d)):
+      count = 0
+      for l, dna in enumerate(Dna):  
+        if week2.AproximatePatternCount(Pattern, dna, d)>=1:
+          count = count +1
+        if count == len(Dna):
+          Patterns.append(Pattern)    
+  
+  Patterns = list(dict.fromkeys(Patterns))
+  return ' '.join(Patterns)
+
+def calcEntropy(vals):
+  entropy = 0
+  for i,val in enumerate(vals):
+    if (val != 0):
+      entropy = entropy + (val * math.log2(val))
+
+  entropy = -entropy
+  return entropy
+
+def MotifEntropy(Motifs):
+  MotifEntropy = []
+  for k in range(len(Motifs[0])):
+    Count = {'A':0,'T':0,'G':0,'C':0}
+    for i in range(len(Motifs)):
+      nt = Motifs[i][k]
+      Count[nt] = Count[nt]+1
+    CountList = Count.values()
+    Profile = [i / len(Motifs) for i in CountList]
+    MotifEntropy.append(calcEntropy(Profile))
+
+  return round(sum(MotifEntropy),4)
+
+
+def DistanceBetweenPatternAndStrings(Pattern, Dna):
+  k = len(Pattern)
+  distance = 0
+  for  i, dna in enumerate(Dna):
+    hammingDistance = float("inf")
+    for j in range(len(dna)-k+1):
+      if hammingDistance > week2.HammingDistance(Pattern, dna[j:j+k]):
+        hammingDistance = week2.HammingDistance(Pattern, dna[j:j+k])
+    distance += hammingDistance
+  return distance
+
+
+def MedianString(Dna, k):
+  distance = float("inf")
+  for i in range(4**k-1):
+    Pattern = week1.NumberToPattern(i, k)
+    if distance > DistanceBetweenPatternAndStrings(Pattern, Dna):
+      distance = DistanceBetweenPatternAndStrings(Pattern, Dna)
+      Median = Pattern
+  return Median
+
+def MotifProfile(Motifs):
+  Profile = {'A':[],'C':[],'G':[],'T':[]}
+  for k in range(len(Motifs[0])):
+    for key in Profile:
+      Profile[key].append(0)
+
+  for k in range(len(Motifs[0])):
+    for i in range(len(Motifs)):
+      nt = Motifs[i][k]
+      for key in Profile:
+        if key == nt:
+          Profile[key][k] += 1    
+        # Profile[key] = [t / len(Motifs) for t in Profile[key]]
+    # CountList = Count.values()
+    # Profile = [i / len(Motifs) for i in CountList]
+  return Profile
+
+def MotifProbability(Motif, Profile):
+  probability = 1
+  for i, nt in enumerate(Motif[::]):
+    probability *= Profile[nt][i]
+  return probability
+
+def MostProbableKmer(Dna, k, Profile):
+  probability = -1
+  Pattern = []
+  for i in range(len(Dna)-k+1):
+    Text = Dna[i:i+k]
+    MProb = MotifProbability(Text, Profile)
+    if MProb > probability:
+      Pattern = []
+      probability = MProb
+      Pattern.append(Text)
+    elif MProb == probability:
+      Pattern.append(Text)
+  if(len(Pattern) == 0):
+    Pattern.append(Dna[0:k])
+  return Pattern[0]
+
+# def Score(Motifs):
+#   k = len(Motifs[0])
+#   distance = float("inf")
+#   for i in range(4**k-1):
+#     Pattern = week1.NumberToPattern(i, k)
+#     dist = DistanceBetweenPatternAndStrings(Pattern, Motifs)
+#     if distance > dist:
+#       distance = dist
+#   return distance
+
+def Score(Motifs):
+  Profile = MotifProfile(Motifs)
+  consensus = ConsesnsusSeq(Profile)
+  distance = DistanceBetweenPatternAndStrings(consensus, Motifs)
+  return distance
+
+def ConsesnsusSeq(Profile):
+  consesus = []
+  for i in range(len(Profile['A'])):
+    freq = 0
+    consesus.append('x')
+    for key in Profile:
+      if Profile[key][i] > freq:
+        freq = Profile[key][i]
+        consesus[i] = key
+  return ''.join(consesus)
+
+def GreedyMotifSearch(DnaTxt, k, t):
+  Dna = DnaTxt.split(' ')
+  BestMotifs = []
+  for i, strand in enumerate(Dna):
+    BestMotifs.append(strand[0:k])
+  for i in range(len(Dna[0])-k+1):
+    Motifs = []
+    Motifs.append(Dna[0][i:i+k])
+    for j in range(1,t):
+      Profile = MotifProfile(Motifs)
+      Motifs.append(MostProbableKmer(Dna[j], k, Profile))
+    if Score(Motifs) < Score(BestMotifs):
+      BestMotifs = Motifs
+  return '\n'.join(BestMotifs)
