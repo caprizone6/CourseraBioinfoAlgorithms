7f20617eb54cccdba295a3a2184bf541aee04bed
Jaimin Patel <caprizone6@gmail.com>
2019-12-16 04:24:41 +0000

    week4 Weighted randomize

--- week4.py
+++ week4.py
@@ -1,25 +1,6 @@
 import week3
 import random
 
-# def RandomizedMotifSearch(Dna, k, t):
-#   # randomly select k-mers Motifs = (Motif1, …, Motift) in each string from Dna
-#   bestscore = float("inf")
-#   while True:
-#     Motifs = []
-#     for  j, dna in enumerate(Dna):
-#       start = random.randint(0,len(dna)-k)
-#       Motifs.append(dna[start:start+k])
-#     # BestMotifs = Motifs
-#     Profile = week3.Motif2Profile(Motifs)
-#     Motifs = week3.Profile2Motif(Dna, k, Profile)
-#     score = week3.Score(Motifs)
-#     if score < bestscore:
-#       print(score)
-#       bestscore = score
-#     else:  
-#       return Motifs
-  # return '\n'.join(BestMotifs)
-
 def RandomizedMotifSearch(Dna, k, t):
   # randomly select k-mers Motifs = (Motif1, …, Motift) in each string from Dna
   Motifs = []
@@ -46,3 +27,11 @@ def run1000(DnaTxt, k, t):
       bestScore = score
       bestMotifs = Motifs
   return bestMotifs
+
+def RandomBiased(Prob):
+  total = sum(Prob)
+  wProb = [i/total for i in Prob]
+  selected = random.choices(Prob, wProb)
+  i = Prob.index(selected[0])
+  return i
+
